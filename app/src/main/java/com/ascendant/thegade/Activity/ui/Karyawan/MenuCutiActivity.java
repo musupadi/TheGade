8067554d17935ceec17211a50d562f5231760762
Zyarga <musupadi159@gmail.com>
2021-09-25 18:25:12 +0700

    Commit

--- app/src/main/java/com/ascendant/thegade/Activity/ui/Karyawan/MenuCutiActivity.java
+++ app/src/main/java/com/ascendant/thegade/Activity/ui/Karyawan/MenuCutiActivity.java
@@ -2,15 +2,96 @@ package com.ascendant.thegade.Activity.ui.Karyawan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.ascendant.thegade.Method.Ascendant;
 import com.ascendant.thegade.R;
 
-public class MenuCutiActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MenuCutiActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+    String Tanggals = "Anak";
+    String tanggalDari = "1994-01-15";
+    String tanggalSampai = "1994-01-15";
+
+    LinearLayout LTanggalDari, LTanggalSampai;
+    TextView TanggalDari, TanggalSampai;
+
+    Ascendant AscNet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_cuti);
+        AscNet = new Ascendant();
+        Declaration();
+        OnClick();
+    }
+    private void OnClick(){
+        LTanggalDari.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Tanggals = "Dari";
+                showDatePicker();
+            }
+        });
+        LTanggalSampai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Tanggals = "Sampai";
+                showDatePicker();
+            }
+        });
+    }
+    private void Declaration(){
+        LTanggalDari = findViewById(R.id.linearTanggalMulai);
+        LTanggalSampai = findViewById(R.id.linearTanggalSelesai);
+        TanggalDari = findViewById(R.id.tvTanggalMulai);
+        TanggalSampai = findViewById(R.id.tvTanggalSelesai);
+        TanggalDari.setText(AscNet.thisDay());
+        TanggalSampai.setText(AscNet.thisDay());
+        tanggalDari = AscNet.Today();
+        tanggalSampai = AscNet.Today();
+    }
+    private void showDatePicker(){
+        DatePickerDialog dialog = new DatePickerDialog(this,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+        dialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        String M = "01";
+        String D = "01";
+        int m = month+1;
+        if (m<10){
+            M="0"+String.valueOf(month+1);
+        }else{
+            M=String.valueOf(month+1);
+        }
+        if (dayOfMonth<10){
+            D="0"+String.valueOf(dayOfMonth);
+        }else{
+            D=String.valueOf(dayOfMonth);
+        }
+        String date = year+"-"+M+"-"+D;
+        if (Tanggals.equals("Dari")){
+            TanggalDari.setText(AscNet.DateChanges(String.valueOf(year),M,D));
+            tanggalDari = date;
+        }else if (Tanggals.equals("Sampai")){
+            TanggalSampai.setText(AscNet.DateChanges(String.valueOf(year),M,D));
+            tanggalSampai = date;
+        }
+
     }
 }
--- app/src/main/java/com/ascendant/thegade/Method/Ascendant.java
+++ app/src/main/java/com/ascendant/thegade/Method/Ascendant.java
@@ -2,6 +2,10 @@ package com.ascendant.thegade.Method;
 
 import android.util.Base64;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Ascendant {
     public String AUTH(){
         String username = "gade-api";
@@ -22,5 +26,77 @@ public class Ascendant {
     public String TimeChanger(String time){
         String Times = time.substring(0,8);
         return Times;
+    }
+    public String Today(){
+        DateFormat dateFormat3 = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
+        String dates = dateFormat3.format(date);
+        return dates;
+    }
+    public static String thisDay(){
+        DateFormat dateFormat1 = new SimpleDateFormat("dd");
+        DateFormat dateFormat2 = new SimpleDateFormat("MM");
+        DateFormat dateFormat3 = new SimpleDateFormat("yyyy");
+        Date date = new Date();
+        String Day = dateFormat1.format(date);
+        String month = dateFormat2.format(date);
+        String Year = dateFormat3.format(date);
+        String MONTH = "Januari";
+        if (month.equals("01") || month.equals("1")){
+            MONTH = "Januari";
+        }else if (month.equals("02") || month.equals("2")){
+            MONTH = "Februari";
+        }else if (month.equals("03") || month.equals("3")){
+            MONTH = "Maret";
+        }else if (month.equals("04") || month.equals("4")){
+            MONTH = "April";
+        }else if (month.equals("05") || month.equals("5")){
+            MONTH = "Mei";
+        }else if (month.equals("06") || month.equals("6")){
+            MONTH = "Juni";
+        }else if (month.equals("07") || month.equals("7")){
+            MONTH = "Juli";
+        }else if (month.equals("08") || month.equals("8")){
+            MONTH = "Agustus";
+        }else if (month.equals("09") || month.equals("9")){
+            MONTH = "September";
+        }else if (month.equals("10")){
+            MONTH = "Oktober";
+        }else if (month.equals("11")){
+            MONTH = "November";
+        }else if (month.equals("12")){
+            MONTH = "Desember";
+        }
+        return Day+"-"+MONTH+"-"+Year;
+    }
+    public  String DateChanges(String year,String month,String day){
+        String MONTH = "Januari";
+        if (month.equals("01") || month.equals("1")){
+            MONTH = "Januari";
+        }else if (month.equals("02") || month.equals("2")){
+            MONTH = "Februari";
+        }else if (month.equals("03") || month.equals("3")){
+            MONTH = "Maret";
+        }else if (month.equals("04") || month.equals("4")){
+            MONTH = "April";
+        }else if (month.equals("05") || month.equals("5")){
+            MONTH = "Mei";
+        }else if (month.equals("06") || month.equals("6")){
+            MONTH = "Juni";
+        }else if (month.equals("07") || month.equals("7")){
+            MONTH = "Juli";
+        }else if (month.equals("08") || month.equals("8")){
+            MONTH = "Agustus";
+        }else if (month.equals("09") || month.equals("9")){
+            MONTH = "September";
+        }else if (month.equals("10")){
+            MONTH = "Oktober";
+        }else if (month.equals("11")){
+            MONTH = "November";
+        }else if (month.equals("12")){
+            MONTH = "Desember";
+        }
+        String Dates = day+"-"+MONTH+"-"+year;
+        return Dates;
     }
 }
