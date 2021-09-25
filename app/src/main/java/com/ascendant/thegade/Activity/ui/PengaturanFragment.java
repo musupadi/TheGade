55671e8db3817c322d0583bcd2709f8ac4dc05ab
Zyarga <musupadi159@gmail.com>
2021-09-25 19:53:09 +0700

    Commit

--- app/src/main/java/com/ascendant/thegade/Activity/ui/PengaturanFragment.java
+++ app/src/main/java/com/ascendant/thegade/Activity/ui/PengaturanFragment.java
@@ -1,60 +1,37 @@
 package com.ascendant.thegade.Activity.ui;
 
+import android.database.Cursor;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.ascendant.thegade.Method.Ascendant;
 import com.ascendant.thegade.R;
+import com.ascendant.thegade.SharedPreferance.DB_Helper;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PengaturanFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PengaturanFragment extends Fragment {
+    Ascendant AscNet;
+    TextView nama,nik;
+    DB_Helper dbHelper;
+    String Id,Username,Nama,Status,Nik;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public PengaturanFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PengaturanFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PengaturanFragment newInstance(String param1, String param2) {
-        PengaturanFragment fragment = new PengaturanFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +39,26 @@ public class PengaturanFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pengaturan, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        nama = view.findViewById(R.id.tvNama);
+        nik = view.findViewById(R.id.tvNik);
+        AscNet = new Ascendant();
+        dbHelper = new DB_Helper(getActivity());
+        Cursor cursor = dbHelper.checkUser();
+        if (cursor.getCount()>0){
+            while (cursor.moveToNext()){
+                Id = cursor.getString(0);
+                Username = cursor.getString(1);
+                Nama = cursor.getString(2);
+                Status = cursor.getString(3);
+                Nik = cursor.getString(4);
+            }
+        }
+        nama.setText(Nama);
+        nik.setText(Nik);
     }
 }
